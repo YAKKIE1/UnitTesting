e27a506e33f96b7c62820c7961c29fb6fed008e1
Sinovuyo <sinovuyomlanjeni137@gmail.com>
2021-04-01 03:19:14 +0200

    after the disabled

---
+++ src/main/java/Assign1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Azania
+ */
+public class Assign1 {
+    public int addMeth(int num1, int num2){
+        return num1 + num2;
+    }
+    public int compare(int a, int b, int c){
+        return a + b + c;
+    }
+
+public int suare(int a){
+return a*a;
+}
+}
